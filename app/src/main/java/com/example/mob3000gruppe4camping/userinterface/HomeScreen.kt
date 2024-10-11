3d405e15544d2d5a391f2215b88640833eebb034
jeskro1 <jeskro@outlook.com>
2024-10-11 16:12:59 +0200

    mangler for det meste booking

--- app/src/main/java/com/example/mob3000gruppe4camping/userinterface/HomeScreen.kt
+++ app/src/main/java/com/example/mob3000gruppe4camping/userinterface/HomeScreen.kt
@@ -1,18 +1,19 @@
 package com.example.mob3000gruppe4camping.userinterface
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.mob3000gruppe4camping.R
 
 @Composable
 fun HomeScreen() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-
-
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -21,11 +22,22 @@ fun HomeScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            CampingImage()
+            Spacer(modifier = Modifier.height(32.dp))
             TitleText()
             Spacer(modifier = Modifier.height(32.dp))
             CenteredButton()
         }
     }
+}
+
+@Composable
+fun CampingImage() {
+    Image(
+        painter = painterResource(id = R.drawable.camping_zone_1), // Replace with your image resource
+        contentDescription = "Camping Logo",
+        modifier = Modifier.size(200.dp) // Adjust size if necessary
+    )
 }
 
 @Composable
