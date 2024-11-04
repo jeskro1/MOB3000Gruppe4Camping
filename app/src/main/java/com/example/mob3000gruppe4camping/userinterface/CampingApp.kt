6fc1f87a3fb83504a7a3bcbdc784eeeab126362c
florsalvas <rolf.salvesen@gmail.com>
2024-11-04 16:52:30 +0100

    Lagt til google maps integrering

--- app/src/main/java/com/example/mob3000gruppe4camping/userinterface/CampingApp.kt
+++ app/src/main/java/com/example/mob3000gruppe4camping/userinterface/CampingApp.kt
@@ -1,9 +1,12 @@
 package com.example.mob3000gruppe4camping.userinterface
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -33,7 +36,7 @@ fun CampingApp() {
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(Screen.Home.route) { HomeScreen(navController) }
-            composable(Screen.Map.route) {MapScreen()}
+            composable(Screen.Map.route) {OpenGoogleMapsLocation()}
             composable(Screen.Profile.route) { ProfilesScreen(navController) }
             composable(Screen.Booking.route) { BookingScreen(navController) }
             composable(Screen.Receipt.route) { ReceiptScreen() }
@@ -41,3 +44,20 @@ fun CampingApp() {
         }
     }
 }
+
+@Composable
+fun OpenGoogleMapsLocation() {
+    val context = LocalContext.current
+    val latitude = 48.8584
+    val longitude = 2.2945
+
+    val uri = Uri.parse("geo:$latitude,$longitude")
+    val intent = Intent(Intent.ACTION_VIEW, uri).apply {
+        setPackage("com.google.android.apps.maps")
+    }
+
+    if (intent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(intent)
+    }
+}
+
